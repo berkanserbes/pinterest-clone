88a10bd02899497ef6413d05ccf31e4a332b5b4e
Berkan Serbes <berkan_serbes@hotmail.com>
2023-07-14 22:22:25 +0300

    Modified UserInfo

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,10 +3,19 @@ import Image from "next/image";
 
 const UserInfo = ({ userInfo }) => {
   return (
-    <div className="text-center">
-      <h1>{userInfo.email}</h1>
-      <h1>{userInfo.userName}</h1>
-      <h1>{userInfo.userImage}</h1>
+    <div className="flex flex-col items-center gap-5">
+      <Image
+        src={userInfo.userImage}
+        alt={`${userInfo.userName} profile photo`}
+        width={100}
+        height={100}
+        className="rounded-full"
+      />
+      <h2 className="font-semibold text-2xl">{userInfo.userName}</h2>
+      <h2 className="text-gray-400">{userInfo.email}</h2>
+      <button className="bg-gray-200 p-3 rounded-full font-semibold">
+        Share
+      </button>
     </div>
   );
 };
