5e93bb8643ae95bfa80cab23d64ae78fb850db3f
Berkan Serbes <berkan_serbes@hotmail.com>
2023-07-18 21:05:27 +0300

    Modified file

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { getFirestore, query, collection, getDocs } from "firebase/firestore";
+import app from "../firebaseConfig";
+
 export default function Home() {
-  return (
-   <div>Pinterest Clone</div>
-  )
+  const db = getFirestore(app);
+  const [allPins, setAllPins] = useState([]);
+
+  useEffect(() => {
+    getAllPins();
+  }, []);
+
+  const getAllPins = async () => {
+    const q = query(collection(db, "pins"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setAllPins((prev) => [...prev, doc.data()]);
+    });
+  };
+
+  return <div className="p-3"></div>;
 }
