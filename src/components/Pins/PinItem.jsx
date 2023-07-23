203bac60e0543d7f9bf717ecfa64c80dc3eac9db
Berkan Serbes <berkan_serbes@hotmail.com>
2023-07-23 12:46:49 +0300

    Modified PinItem.jsx

--- src/components/Pins/PinItem.jsx
+++ src/components/Pins/PinItem.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const PinItem = ({ item }) => {
+  const router = useRouter();
+
   return (
-    <div className="text-center">
-      <Image src={item.image} alt={item.title} height={500} width={500} />
+    <div className="text-center border border-gray-500">
+      <div onClick={() => router.push(`/pin/${item.id}`)}>
+        <Image
+          src={item.image}
+          alt={item.title}
+          height={500}
+          width={500}
+          className="rounded-3xl cursor-pointer"
+        />
+      </div>
       <h1>{item.title}</h1>
       <h2>{item.description}</h2>
     </div>
